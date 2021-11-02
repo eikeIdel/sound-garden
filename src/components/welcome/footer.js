33412463ed06b22bfd5334c5f23a61f33390e343
Berd Sena <sena.berd@gmail.com>
2021-11-02 13:34:05 +0100

    Adding normalize.css

--- src/components/welcome/footer.js
+++ src/components/welcome/footer.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import './footer.css';
+import React from "react";
+import "./footer.css";
 
 const Footer = () => {
-    return (
-        <footer>
-            <div>
-                <div className="social">
-                    <i class="">A</i>
-                    <i class="">B</i>
-                    <i class="">C</i>
-                    <i class="">D</i>
-                </div>
-                <div className="copyright">
-                    Copyright &copy; 2021. Sound Garden
-                </div>
-            </div>
-        </footer>  
-    );
-}
+  return (
+    <footer>
+      <div>
+        <div className="social">
+          <i class="">A</i>
+          <i class="">B</i>
+          <i class="">C</i>
+          <i class="">D</i>
+        </div>
+        <div className="copyright">Copyright &copy; 2021. Sound Garden</div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
