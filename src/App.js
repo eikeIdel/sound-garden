03372bd5476b643989db68de2554ad2c2230d699
Eike Idel <88095420+eikeIdel@users.noreply.github.com>
2021-11-11 14:34:42 +0100

    save something 2

--- src/App.js
+++ src/App.js
@@ -7,9 +7,16 @@ import Navigation from './components/Router/Router.jsx';
 function App() {
   return (
     <div className="App">
-
+       < Link to="/">Welcome</Link>
+             <br/>
+            < Link to="/HouseA_Tropical">House A - Tropical</Link>
+            <br/>
+            < Link to="/HouseB_Aroids">House B - Aroids</Link>
+      <Router>
+      <Switch>
       <Navigation/>
-
+      </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -6,15 +6,7 @@ import Map from '../07-08-map-responsive-layout';
 
 export default function Navigation() {
     return (
-        <Router>
-            < Link to="/">Welcome</Link>
-             <br/>
-            < Link to="/HouseA_Tropical">House A - Tropical</Link>
-            <br/>
-            < Link to="/HouseB_Aroids">House B - Aroids</Link>
-            
-
-            <Switch>
+        <>
                 <Route exact path="/">
                     <Welcome/>
                 </Route>
@@ -86,8 +78,6 @@ export default function Navigation() {
                 </Route>
 
             
-
-            </Switch>
-        </Router>
+</>
     )
 }
