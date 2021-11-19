521630f8d676a9e2bffff32332994e13b569e598
Berd <86966517+berdsena@users.noreply.github.com>
2021-11-19 01:21:21 +0100

    included import of quiz_icon

--- src/components/welcome/navbar.js
+++ src/components/welcome/navbar.js
@@ -4,6 +4,7 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import logo from "./assets/logo_bo_small.png";
+import quiz from "./assets/quiz_icon.png";
 
 const NavigationBar = () => {
   return (
